653a9712f5923b774fb612a7aca66db10be78b2f
root <dennys1@hotmail.com>
2020-11-02 13:53:23 +0000

    Cambios para release1

--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -1,1 +1,18 @@
+When("Inicio el Ahorcado") do
+    visit '/'
+end
+  
+Then("muestra {string}") do |titulo|
+    expect(page).to have_css "h1", text: titulo
+end
+
+Given("Inicio la aplicación con un jugador") do
+    visit '/'
+end
+
+  When("arriesga con palabra {string}") do |palabra|
+    fill_in "palabra", :with => palabra
+    click_button "Arriesgar"
+  end
+
 
